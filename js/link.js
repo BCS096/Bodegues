a609e79044b1fc17b4979134bf08261e8df0802e
BCS096 <tomeucs99@gmail.com>
2023-03-17 18:04:30 +0100

    todo lo hecho pasado a javascript

--- js/link.js
+++ js/link.js
@@ -3,6 +3,7 @@ window.onload = function () {
     aux.href = "javascript:initButtons()";
     crearCarouselPrincipal();
     filtrado();
+    listadoCardXL();
 }
 
 function initButtons() {
@@ -336,66 +337,265 @@ function filtrado() {
 }
 
 function listadoCardXL() {
+    const dad = document.getElementById("seccion");
     const container1 = document.createElement("div");
     container1.className = "container granate aplicar-borde margen";
 
-    const card1 = document.createElement("div");
-    card1.className = "card-body";
+    for (let i = 0; i < 3; i++) {
+        const card1 = document.createElement("div");
+        card1.className = "card-body";
 
-    const containerCard = document.createElement("div");
-    containerCard.className = "container granate aplicar-borde margen";
+        const containerCard = document.createElement("div");
+        containerCard.className = "container granate aplicar-borde margen";
 
-    const wrapper = document.createElement("div");
-    wrapper.className = "wrapper row gris aplicar-borde";
+        const wrapper = document.createElement("div");
+        wrapper.className = "wrapper row gris aplicar-borde";
 
-    const preview = document.createElement("div");
-    preview.className = "preview col-md-6 margen";
+        const preview = document.createElement("div");
+        preview.className = "preview col-md-6 margen";
 
-    const previewPic = document.createElement("div");
-    previewPic.classList.add("preview-pic", "tab-content");
+        const previewPic = document.createElement("div");
+        previewPic.classList.add("preview-pic", "tab-content", "margen");
 
-    const pic1 = document.createElement("div");
-    pic1.classList.add("tab-pane", "active");
-    pic1.id = "pic-1";
-    const pic1Img = document.createElement("img");
-    pic1Img.src = "http://placekitten.com/400/252";
-    pic1.appendChild(pic1Img);
+        const pic1 = document.createElement("div");
+        pic1.classList.add("tab-pane", "active");
+        pic1.id = "pic-1";
+        const pic1Img = document.createElement("img");
+        pic1Img.setAttribute("src", "http://placekitten.com/400/252");
+        pic1.appendChild(pic1Img);
 
-    const pic2 = document.createElement("div");
-    pic2.classList.add("tab-pane");
-    pic2.id = "pic-2";
-    const pic2Img = document.createElement("img");
-    pic2Img.src = "http://placekitten.com/400/252";
-    pic2.appendChild(pic2Img);
+        const pic2 = document.createElement("div");
+        pic2.classList.add("tab-pane");
+        pic2.id = "pic-2";
+        const pic2Img = document.createElement("img");
+        pic2Img.src = "http://placekitten.com/400/252";
+        pic2.appendChild(pic2Img);
 
-    const pic3 = document.createElement("div");
-    pic3.classList.add("tab-pane");
-    pic3.id = "pic-3";
-    const pic3Img = document.createElement("img");
-    pic3Img.src = "http://placekitten.com/400/252";
-    pic3.appendChild(pic3Img);
+        const pic3 = document.createElement("div");
+        pic3.classList.add("tab-pane");
+        pic3.id = "pic-3";
+        const pic3Img = document.createElement("img");
+        pic3Img.src = "http://placekitten.com/400/252";
+        pic3.appendChild(pic3Img);
 
-    const pic4 = document.createElement("div");
-    pic4.classList.add("tab-pane");
-    pic4.id = "pic-4";
-    const pic4Img = document.createElement("img");
-    pic4Img.src = "http://placekitten.com/400/252";
-    pic4.appendChild(pic4Img);
+        const pic4 = document.createElement("div");
+        pic4.classList.add("tab-pane");
+        pic4.id = "pic-4";
+        const pic4Img = document.createElement("img");
+        pic4Img.src = "http://placekitten.com/400/252";
+        pic4.appendChild(pic4Img);
 
-    const pic5 = document.createElement("div");
-    pic5.classList.add("tab-pane");
-    pic5.id = "pic-5";
-    const pic5Img = document.createElement("img");
-    pic5Img.src = "http://placekitten.com/400/252";
-    pic5.appendChild(pic5Img);
+        const pic5 = document.createElement("div");
+        pic5.classList.add("tab-pane");
+        pic5.id = "pic-5";
+        const pic5Img = document.createElement("img");
+        pic5Img.src = "http://placekitten.com/400/252";
+        pic5.appendChild(pic5Img);
 
-    previewPic.appendChild(pic1);
-    previewPic.appendChild(pic2);
-    previewPic.appendChild(pic3);
-    previewPic.appendChild(pic4);
-    previewPic.appendChild(pic5);
+        previewPic.appendChild(pic1);
+        previewPic.appendChild(pic2);
+        previewPic.appendChild(pic3);
+        previewPic.appendChild(pic4);
+        previewPic.appendChild(pic5);
 
-    preview.appendChild(previewpic);
+        preview.appendChild(previewPic);
 
+        const previewThumbnail = document.createElement("ul");
+        previewThumbnail.classList.add("preview-thumbnail", "nav", "nav-tabs", "margen");
+
+        const pic1Li = document.createElement("li");
+        pic1Li.classList.add("active");
+        const pic1Link = document.createElement("a");
+        pic1Link.setAttribute("data-target", "#pic-1");
+        pic1Link.setAttribute("data-toggle", "tab");
+        const pic1Img1 = document.createElement("img");
+        pic1Img1.src = "http://placekitten.com/200/126";
+        pic1Link.appendChild(pic1Img1);
+        pic1Li.appendChild(pic1Link);
+
+        const pic2Li = document.createElement("li");
+        const pic2Link = document.createElement("a");
+        pic2Link.setAttribute("data-target", "#pic-2");
+        pic2Link.setAttribute("data-toggle", "tab");
+        const pic2Img1 = document.createElement("img");
+        pic2Img1.src = "http://placekitten.com/200/126";
+        pic2Link.appendChild(pic2Img1);
+        pic2Li.appendChild(pic2Link);
+
+        const pic3Li = document.createElement("li");
+        const pic3Link = document.createElement("a");
+        pic3Link.setAttribute("data-target", "#pic-3");
+        pic3Link.setAttribute("data-toggle", "tab");
+        const pic3Img1 = document.createElement("img");
+        pic3Img1.src = "http://placekitten.com/200/126";
+        pic3Link.appendChild(pic3Img1);
+        pic3Li.appendChild(pic3Link);
+
+        const pic4Li = document.createElement("li");
+        const pic4Link = document.createElement("a");
+        pic4Link.setAttribute("data-target", "#pic-4");
+        pic4Link.setAttribute("data-toggle", "tab");
+        const pic4Img1 = document.createElement("img");
+        pic4Img1.src = "http://placekitten.com/200/126";
+        pic4Link.appendChild(pic4Img1);
+        pic4Li.appendChild(pic4Link);
+
+        const pic5Li = document.createElement("li");
+        const pic5Link = document.createElement("a");
+        pic5Link.setAttribute("data-target", "#pic-5");
+        pic5Link.setAttribute("data-toggle", "tab");
+        const pic5Img1 = document.createElement("img");
+        pic5Img1.src = "http://placekitten.com/200/126";
+        pic5Link.appendChild(pic5Img1);
+        pic5Li.appendChild(pic5Link);
+
+        previewThumbnail.appendChild(pic1Li);
+        previewThumbnail.appendChild(pic2Li);
+        previewThumbnail.appendChild(pic3Li);
+        previewThumbnail.appendChild(pic4Li);
+        previewThumbnail.appendChild(pic5Li);
+
+        preview.appendChild(previewPic);
+        preview.appendChild(previewThumbnail);
+
+        const detailsDiv = document.createElement('div');
+        detailsDiv.classList.add('details', 'col-md-6', 'margen','divPadre');
+
+        const titleH3 = document.createElement('h3');
+        titleH3.classList.add('product-title', 'letras','divHijo');
+        titleH3.textContent = "men's shoes fashion";
+        detailsDiv.appendChild(titleH3);
+
+        const ratingDiv = document.createElement('div');
+        ratingDiv.classList.add('rating','divHijo');
+        const starsDiv = document.createElement('div');
+        starsDiv.classList.add('stars');
+        const checkedStarSpan1 = document.createElement('span');
+        checkedStarSpan1.classList.add('fa', 'fa-star', 'checked');
+        const checkedStarSpan2 = document.createElement('span');
+        checkedStarSpan2.classList.add('fa', 'fa-star', 'checked');
+        const checkedStarSpan3 = document.createElement('span');
+        checkedStarSpan3.classList.add('fa', 'fa-star', 'checked');
+        const uncheckedStarSpan1 = document.createElement('span');
+        uncheckedStarSpan1.classList.add('fa', 'fa-star');
+        const uncheckedStarSpan2 = document.createElement('span');
+        uncheckedStarSpan2.classList.add('fa', 'fa-star');
+        starsDiv.appendChild(checkedStarSpan1);
+        starsDiv.appendChild(checkedStarSpan2);
+        starsDiv.appendChild(checkedStarSpan3);
+        starsDiv.appendChild(uncheckedStarSpan1);
+        starsDiv.appendChild(uncheckedStarSpan2);
+        ratingDiv.appendChild(starsDiv);
+        const reviewSpan = document.createElement('span');
+        reviewSpan.classList.add('review-no', 'letras');
+        reviewSpan.textContent = '41 reviews';
+        ratingDiv.appendChild(reviewSpan);
+        detailsDiv.appendChild(ratingDiv);
+
+        const descriptionP = document.createElement('p');
+        descriptionP.classList.add('product-description', 'letras','divHijo');
+        descriptionP.textContent = 'Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.';
+        detailsDiv.appendChild(descriptionP);
+
+        const priceH4 = document.createElement('h4');
+        priceH4.classList.add('price', 'letras','divHijo');
+        priceH4.textContent = 'current price: ';
+        const priceSpan = document.createElement('span');
+        priceSpan.textContent = '$180';
+        priceH4.appendChild(priceSpan);
+        detailsDiv.appendChild(priceH4);
+
+        const voteP = document.createElement('p');
+        voteP.classList.add('vote', 'letras','divHijo');
+        const strong1 = document.createElement('strong');
+        strong1.textContent = '91%';
+        voteP.appendChild(strong1);
+        voteP.appendChild(document.createTextNode(' of buyers enjoyed this product! '));
+        const strong2 = document.createElement('strong');
+        strong2.textContent = '(87 votes)';
+        voteP.appendChild(strong2);
+        detailsDiv.appendChild(voteP);
+
+        const sizesH5 = document.createElement('h5');
+        sizesH5.classList.add('sizes', 'bi-layout-text-sidebar-reverse', 'letras','divHijo');
+        sizesH5.textContent = 'sizes: ';
+        const sizeSpan1 = document.createElement('span');
+        sizeSpan1.classList.add('size', 'letras');
+        sizeSpan1.setAttribute('data-toggle', 'tooltip');
+        sizeSpan1.setAttribute('title', 'small');
+        sizeSpan1.textContent = 's';
+        const sizeSpan2 = document.createElement('span');
+        sizeSpan2.classList.add('size', 'letras');
+        sizeSpan2.setAttribute('data-toggle', 'tooltip');
+        sizeSpan2.setAttribute('title', 'medium');
+        sizeSpan2.textContent = 'm';
+        const sizeSpan3 = document.createElement('span');
+        sizeSpan3.classList.add('size', 'letras');
+        sizeSpan3.setAttribute('data-toggle', 'tooltip');
+        sizeSpan3.setAttribute('title', 'large');
+        sizeSpan3.textContent = 'l';
+        const sizeSpan4 = document.createElement('span');
+        sizeSpan4.classList.add('size', 'letras');
+        sizeSpan4.setAttribute('data-toggle', 'tooltip');
+        sizeSpan4.setAttribute('title', 'large');
+        sizeSpan4.textContent = 'xl';
+
+        sizesH5.appendChild(sizeSpan1);
+        sizesH5.appendChild(sizeSpan2);
+        sizesH5.appendChild(sizeSpan3);
+        sizesH5.appendChild(sizeSpan4);
+
+        detailsDiv.appendChild(sizesH5);
+
+        const sizesH51 = document.createElement('h5');
+        sizesH51.classList.add('colors', 'letras','divHijo');
+        sizesH51.textContent = 'colors:';
+
+        const span1 = document.createElement('span');
+        span1.classList.add('color', 'orange');
+        span1.setAttribute('data-toggle', 'tooltip');
+        span1.setAttribute('title', 'Not in store');
+
+        const span2 = document.createElement('span');
+        span2.classList.add('color', 'green');
+
+        const span3 = document.createElement('span');
+        span3.classList.add('color', 'blue');
+
+        sizesH51.appendChild(span1);
+        sizesH51.appendChild(span2);
+        sizesH51.appendChild(span3);
+
+        detailsDiv.appendChild(sizesH51);
+
+        const finalDiv = document.createElement('div');
+        finalDiv.classList.add('action','divHijo');
+
+        const boton1 = document.createElement('button');
+        boton1.classList.add('add-to-cart');
+        boton1.type = 'button';
+        boton1.textContent = 'add to cart';
+
+        const boton2 = document.createElement('button');
+        boton2.classList.add('like', 'btn', 'btn-default');
+        boton2.type = 'button';
+
+        const spanboton = document.createElement('span');
+        spanboton.classList.add('fa', 'fa-heart');
+
+        boton2.appendChild(spanboton);
+
+        finalDiv.appendChild(boton1);
+        finalDiv.appendChild(boton2);
+
+        detailsDiv.appendChild(finalDiv);
+        wrapper.appendChild(preview);
+        wrapper.appendChild(detailsDiv);
+        containerCard.appendChild(wrapper);
+        card1.appendChild(containerCard);
+        container1.appendChild(card1);
+    }
+
+    dad.appendChild(container1);
 
 }
